a247e9884628e2ed8bfa7d531ab2497a5fdd17ef
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-15 21:30:35 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var num1 = +prompt("Enter anynumber");
+var num2 = +prompt("Enter another number");
+
+var add = num1 + num2;
+document.write(add + "<b>" + " Add" + "</b>" + "<br>");
+
+var sub = num1 - num2;
+document.write(sub + "<b>" + " Subtract" + "</b>" + "<br>");
+
+var mul = num1 * num2;
+document.write(mul + "<b>" + " Multifly" + "</b>" + "<br>");
+
+var mod = num1 % num2;
+document.write(mod + "<b>" + " Modules" + "</b>");
